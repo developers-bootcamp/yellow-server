3d0d3cb51af1194db5de333f2e0c56c87a8e1f7d
AyalaZisman <136646034+AyalaZisman@users.noreply.github.com>
2023-06-18 19:33:43 +0300

    Create Spring boot app and push to github

---
+++ src/main/java/com/yellow/ordermanageryellow/OrderManagerYellowApplication.java
@@ -0,0 +1,13 @@
+package com.yellow.ordermanageryellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagerYellowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerYellowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yellow/ordermanageryellow/OrderManagerYellowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yellow.ordermanageryellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerYellowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
